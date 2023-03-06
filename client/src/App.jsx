422aaa08159c9b91f25146e121b11be5ba1fa41c
arrryan05 <mishra2001aryan@gmail.com>
2023-03-07 00:10:17 +0530

    final session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { BrowserRouter,Link,Route,Routes } from 'react-router-dom'
-import {logo} from './assets';
-import {Home,CreatePost} from './pages';
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
 
-const App = () => {
-    <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
@@ -15,12 +15,11 @@ const App = () => {
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
-        <Route path="/" element = {<Home/>}/>
-        <Route path="/create-post" element = {<CreatePost/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </main>
-    </BrowserRouter>
+  </BrowserRouter>
+);
 
-}
-
-export default App 
+export default App;
